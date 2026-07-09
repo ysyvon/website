945e1fb7cdb0b8fe1b8e21c2a2bdd9fcfc2c7779
Ys <121690001+ysyvon@users.noreply.github.com>
2026-07-08 23:29:59 -0400

    Convert journal to static image posts

--- journal-data.js
+++ journal-data.js
@@ -1,9 +1,19 @@
-// Edit these slides to add or change images and captions for the Journal page.
-// Each slide should point to an image in your site folder or an external image URL.
-window.JOURNAL_SLIDES = [
+// Add new image-based journal posts here. Newest posts display first.
+// Put images in journal-assets/ and use a date in YYYY-MM-DD format for sorting.
+window.JOURNAL_POSTS = [
   {
-    src: './journal-assets/moodboard_8_jul_2026_ys.png',
-    alt: 'Moodboard journal image for 8 July 2026',
-    caption: '8 july 2026'
+    date: '2026-07-08',
+    dateLabel: '8 july 2026',
+    image: './journal-assets/moodboard_8_jul_2026_ys.png',
+    alt: 'Moodboard journal page for 8 July 2026',
+    caption: ''
   }
 ];
+
+window.JOURNAL_SLIDES = window.JOURNAL_POSTS.map(function (post) {
+  return {
+    src: post.image,
+    alt: post.alt,
+    caption: post.dateLabel
+  };
+});
